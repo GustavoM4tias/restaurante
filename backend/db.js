26c43baa164af8401148a0c1d86f8b08499bc47c
Gustavo Henrique Matias Diniz <faculdogu@gmail.com>
2025-02-21 23:46:29 -0300

    adjust pool mysql

--- backend/db.js
+++ backend/db.js
@@ -1,21 +1,25 @@
-import mysql from "mysql2";
-import dotenv from "dotenv";
+import mysql from 'mysql2';
+import dotenv from 'dotenv';
 
 dotenv.config(); // Carrega variáveis de ambiente do `.env`
 
-const connection = mysql.createConnection({
-  host: process.env.DB_HOST || "127.0.0.1",
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASS || "12345678",
-  database: process.env.DB_NAME || "restaurante",
+const pool = mysql.createPool({
+  host: process.env.DB_HOST || '127.0.0.1',
+  user: process.env.DB_USER || 'root',
+  password: process.env.DB_PASS || '12345678',
+  database: process.env.DB_NAME || 'restaurante',
+  waitForConnections: true,  // Espera por conexões disponíveis
+  connectionLimit: 10, // Número máximo de conexões simultâneas
+  queueLimit: 0 // Limite da fila de conexões
 });
 
-connection.connect((err) => {
+// Exemplo de uso de pool
+pool.query('SELECT * FROM usuarios', (err, results) => {
   if (err) {
-    console.error("[DB] Erro ao conectar no MySQL:", err);
+    console.error('[DB] Erro na consulta:', err);
   } else {
-    console.log("[DB] Conectado ao MySQL!");
+    console.log('[DB] Dados:', results);
   }
 });
 
-export default connection;
+export default pool;
