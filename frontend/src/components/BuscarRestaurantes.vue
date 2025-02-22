f93a24a3c393e374b3a876d334f150945b0acfd5
Gustavo Henrique Matias Diniz <faculdogu@gmail.com>
2025-02-22 00:22:25 -0300

    api adjust parameters and card test 2

--- frontend/src/components/BuscarRestaurantes.vue
+++ frontend/src/components/BuscarRestaurantes.vue
@@ -39,7 +39,7 @@
 
 <script setup>
 import { ref } from 'vue';
-import { useRestaurantesStore } from '@/stores/restaurantes';
+import { useRestaurantesStore } from '../stores/restaurante';
 
 const restaurantesStore = useRestaurantesStore();
 
